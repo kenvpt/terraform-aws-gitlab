5cbb8092202ae1e36295ae980474e62908d7aa62
Cloud User <centos@ip-172-31-20-77.eu-west-1.compute.internal>
2020-05-09 03:17:50 +0000

    push

--- gitlab_module.tf
+++ gitlab_module.tf
@@ -1,5 +1,5 @@
 module "gitlab1" {
-    source = "/home/centos/terraform-iaac-ken/module_gitlab/gitlab_project"
+    source = "/home/centos/terraform-aws-gitlab/gitlab_project"
     region = "us-west-1"
     vpc_cidr = "192.168.0.0/16"
     public_subnet_cidr = [
@@ -16,8 +16,8 @@ module "gitlab1" {
     instance_type = "t2.xlarge"
     key_name = "my_key"
     public_key_path = "~/.ssh/id_rsa.pub"
-    domain_name = "www.gitlab-prod.kenvpt.com"
-    delegation_set = "Z10047533N9ED5L3STY30"
+    # domain_name = "www.gitlab-prod.kenvpt.com"
+    # delegation_set = "Z10047533N9ED5L3STY30"
     tags = {
      Environment = "Gitlab"
      Department = "Prod"
@@ -27,32 +27,32 @@ module "gitlab1" {
     }
     private_key_path = "~/.ssh/id_rsa"  
 }
-module "gitlab2" {
-    source = "/home/centos/terraform-iaac-ken/module_gitlab/gitlab_project"
-    region = "us-east-2"
-    vpc_cidr = "11.0.0.0/16"
-    public_subnet_cidr = [
-        "11.0.1.0/24",
-        "11.0.2.0/24",
-        "11.0.3.0/24"
-]
-    private_subnet_cidr = [
-        "11.0.11.0/24",
-        "11.0.12.0/24",
-        "11.0.13.0/24"
-    ]
+# module "gitlab2" {
+#     source = "/home/centos/terraform-iaac-ken/module_gitlab/gitlab_project"
+#     region = "us-east-2"
+#     vpc_cidr = "11.0.0.0/16"
+#     public_subnet_cidr = [
+#         "11.0.1.0/24",
+#         "11.0.2.0/24",
+#         "11.0.3.0/24"
+# ]
+#     private_subnet_cidr = [
+#         "11.0.11.0/24",
+#         "11.0.12.0/24",
+#         "11.0.13.0/24"
+#     ]
 
-    instance_type = "t2.xlarge"
-    key_name = "my_key"
-    public_key_path = "~/.ssh/id_rsa.pub"
-    domain_name = "www.gitlab-dev.kenvpt.com"
-    delegation_set = "Z10047533N9ED5L3STY30"
-    tags = {
-     Environment = "Gitlab"
-     Department = "Dev"
-     Team = "Infrastructure"
-     Created_by = "Team5"
+#     instance_type = "t2.xlarge"
+#     key_name = "my_key"
+#     public_key_path = "~/.ssh/id_rsa.pub"
+#     domain_name = "www.gitlab-dev.kenvpt.com"
+#     delegation_set = "Z10047533N9ED5L3STY30"
+#     tags = {
+#      Environment = "Gitlab"
+#      Department = "Dev"
+#      Team = "Infrastructure"
+#      Created_by = "Team5"
 
-    }
-    private_key_path = "~/.ssh/id_rsa"  
-}
+#     }
+#     private_key_path = "~/.ssh/id_rsa"  
+# }
